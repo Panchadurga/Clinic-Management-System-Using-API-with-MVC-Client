e102d6c16792aa2e6454509a6b7b01b7e2ff7a8a
Pancha Durga V <panchadurga@whiteblue.in>
2021-10-20 14:08:23 +0530

    Modified files

--- CMS/CMSUnitTestProject/UnitTest1.cs
+++ CMS/CMSUnitTestProject/UnitTest1.cs
@@ -35,34 +35,34 @@ namespace CMSUnitTestProject
             mockpprovider.Setup(r => r.GetPatientById(1)).Returns(p1);
 
         }
-       
-    //    [Test]
-    //    public void GetDoctorbyIdTest()
-    //    {
-    //        Mock<IDoctor> mockdrepo = new Mock<IDoctor>();
-    //        Mock<IDocProvider> mockdprovider = new Mock<IDocProvider>();
-    //        DocProvider d = new DocProvider(mockdrepo.Object);
-    //        mockdrepo.Setup(r => r.GetDoctorById(1)).Returns(d1);
-    //        mockdprovider.Setup(r => r.GetDoctorById(1)).Returns(d1);
-    //        DoctorsController dc = new DoctorsController(mockdprovider.Object);
 
-    //        var obj = dc.GetDoctorById(1) as OkObjectResult;
-    //        Assert.AreEqual(200, obj.StatusCode);
+        [Test]
+        public void GetDoctorbyIdTest()
+        {
+            Mock<IDoctor> mockdrepo = new Mock<IDoctor>();
+            Mock<IDocProvider> mockdprovider = new Mock<IDocProvider>();
+            DocProvider d = new DocProvider(mockdrepo.Object);
+            mockdrepo.Setup(r => r.GetDoctorById(1)).Returns(d1);
+            mockdprovider.Setup(r => r.GetDoctorById(1)).Returns(d1);
+            DoctorsController dc = new DoctorsController(mockdprovider.Object);
 
-    //    }
-    //    [Test]
-    //    public void GetPatientbyIdTest()
-    //    {
-    //        Mock<IPatient> mockprepo = new Mock<IPatient>();
-    //        Mock<IPatProvider> mockpprovider = new Mock<IPatProvider>();
-    //        PatientProvider p = new PatientProvider(mockprepo.Object);
-    //        mockprepo.Setup(r => r.GetPatientById(1)).Returns(p1);
-    //        mockpprovider.Setup(r => r.GetPatientById(1)).Returns(p1);
-    //        PatientsController pc = new PatientsController(mockpprovider.Object);
+            var obj = dc.GetDoctorById(1) as OkObjectResult;
+            Assert.AreEqual(200, obj.StatusCode);
 
-    //        var obj = pc.GetPatientById(1) as OkObjectResult;
-    //        Assert.AreEqual(200, obj.StatusCode);
+        }
+        //    [Test]
+        //    public void GetPatientbyIdTest()
+        //    {
+        //        Mock<IPatient> mockprepo = new Mock<IPatient>();
+        //        Mock<IPatProvider> mockpprovider = new Mock<IPatProvider>();
+        //        PatientProvider p = new PatientProvider(mockprepo.Object);
+        //        mockprepo.Setup(r => r.GetPatientById(1)).Returns(p1);
+        //        mockpprovider.Setup(r => r.GetPatientById(1)).Returns(p1);
+        //        PatientsController pc = new PatientsController(mockpprovider.Object);
 
-    //    }
+        //        var obj = pc.GetPatientById(1) as OkObjectResult;
+        //        Assert.AreEqual(200, obj.StatusCode);
+
+        //    }
     }
 }
